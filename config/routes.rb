bcd61a8633bdaa165d308857174a7472491cd04e
depressdog <Dakhnovskayaa>
2019-08-09 15:00:13 +0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'home#index'
+end
