4ed2d519702e7f883ad9fe2441ca95346c8229c5
depressdog <Dakhnovskayaa>
2019-08-09 16:51:51 +0300

    done deleting for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,6 +59,7 @@ class UsersController < ApplicationController
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
+
   end
 
   private
