2651c2aaff221d5d3c689cd03c0907d4058e2dbd
depressdog <Dakhnovskayaa>
2019-08-21 10:01:40 +0300

    done cabel(maybe)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,11 @@ class CommentsController < ApplicationController
   before_action :user_is_confirmed, only: [:create, :update, :destroy]
   before_action :set_comment, only: [:update, :destroy]
 
-  # after_action :create, (CommentBroadcastJob.perform_later self)
-  after_commit CommentBroadcastJob.perform_later self, :on => :create
-
-
   def create
     @comment = current_user.comments.create(comment_params)
     if @comment.save
       redirect_to post_path(@comment.post_ident)
+      CommentBroadcastJob.perform_later
     else
       redirect_to post_path(@comment.post_ident), notice: 'Cant be blank'
     end
@@ -19,12 +16,14 @@ class CommentsController < ApplicationController
   def update
     if @comment.update(comment_params)
       redirect_to post_path(@comment.post_ident)
+      CommentBroadcastJob.perform_later
     else
       render post_path(@comment.post_ident)
     end
   end
   def destroy
     @comment.destroy
+    CommentBroadcastJob.perform_later
     redirect_to post_path(@comment.post_ident)
   end
   def destroy_image
