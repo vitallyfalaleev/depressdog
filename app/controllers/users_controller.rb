5956b8c02d0cb95c9196412d1dedc766103c4536
depressdog <Dakhnovskayaa>
2019-08-22 16:07:53 +0300

    done Users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.paginate(page: params[:page], per_page: 5)
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   # GET /users/1
