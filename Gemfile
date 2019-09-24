44fe49401b29d16b8e99be11f3bc65cce355b16a
Vitally Falaleev <dante31.3d@gmail.com>
2019-09-24 17:26:53 +0300

    work

--- Gemfile
+++ Gemfile
@@ -90,3 +90,4 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 gem 'pusher'
 gem 'faker'
+gem 'sendgrid-ruby'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 465,
+      user_name: ENV['gmail_username'],
+      password: ENV['gmail_password'],
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
 end
