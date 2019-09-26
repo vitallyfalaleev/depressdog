7a1d131ab3ab95bda5d4d77f7e354268751d917c
Vitally Falaleev <dante31.3d@gmail.com>
2019-09-26 10:42:08 +0300

    carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+
   include Cloudinary::CarrierWave
-  # process :convert => 'png'
+
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
