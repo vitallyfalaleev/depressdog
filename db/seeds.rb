b25b235ae615af5039850d2144ff4c7f0edeae81
Vitally Falaleev <dante31.3d@gmail.com>
2019-09-27 14:38:29 +0300

    done seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,43 +12,43 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-50.times do
+20.times do
   User.create(name: Faker::Games::Witcher.character,
               email: Faker::Internet.unique.email,
               image_url: Faker::Avatar.image,
               password: '111111a',
               email_confirmed: true)
 end
-50.times do
+10.times do
   Post.create(title: Faker::ProgrammingLanguage.name,
-              body: Faker::Lorem.paragraph(sentence_count: 3),
+              body: Faker::Lorem.paragraph(sentence_count: 25, supplemental: false, random_sentences_to_add: 4),
               user_id: User.all.order('RANDOM()').first.id)
 end
-100.times do
+5.times do
   post = Post.all.order('RANDOM()').first
-  Image.create(remote_image_url: Faker::Avatar.image,
+  Image.create(remote_image_url: Faker::LoremPixel.image,
                post_id: post.id,
                user_id: post.user_id)
 end
-201.times do
+20.times do
   Comment.create(body: Faker::TvShows::DrWho.quote,
-                 remote_image_url: Faker::Avatar.image,
+                 remote_image_url: Faker::LoremPixel.image,
                  commentable_id: Post.all.order('RANDOM()').first.id,
                  commentable_type: 'Post',
                  user_id: User.all.order('RANDOM()').first.id)
 end
-301.times do
+30.times do
   Comment.create(body: Faker::Games::Witcher.quote,
                  commentable_id: Comment.all.order('RANDOM()').first.id,
                  commentable_type: 'Comment',
                  user_id: User.all.order('RANDOM()').first.id)
 end
-300.times do
+10.times do
   Like.create(likable_id: Post.all.order('RANDOM()').first.id,
               likable_type: 'Post',
               user_id: User.all.order('RANDOM()').first.id)
 end
-500.times do
+30.times do
   Like.create(likable_id: Comment.all.order('RANDOM()').first.id,
               likable_type: 'Comment',
               user_id: User.all.order('RANDOM()').first.id)
