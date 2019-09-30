848fc9802346b07c39a386e3adbec8268a82c858
Vitally Falaleev <dante31.3d@gmail.com>
2019-09-30 11:45:22 +0300

    change

--- config/environment.rb
+++ config/environment.rb
@@ -8,9 +8,9 @@ Rails.application.initialize!
 Time::DATE_FORMATS[:ru_datetime] = '%Y.%m.%d в %k:%M:%S'
 
 ActionMailer::Base.smtp_settings = {
-    :user_name => ENV["GG_USERNAME"],
-    :password => ENV["GG_PASSWORD"],
-    :domain => ENV["GG_MAIL"],
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :domain => ENV["SENDGRID_MAIL"],
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
